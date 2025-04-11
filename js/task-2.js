8dd25e13da64f04742ec26ab0432e5bbc87c11d9
AlDemon <all.demmonn@gmail.com>
2025-04-11 23:58:38 +0300

    add hw task-2, modify the file index.html

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,17 @@
-// function getShippingMessage(country, price, deliveryFee) {
-// 	const totalPrice = price + deliveryFee;
-// 	const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-// 	// console.log(message);
-// 	return message;
-// }
+function formatMessage(message, maxLength) {
+	let line = message.length;
+	if (line <= maxLength) {
+		return message;
+	} else {
+		let overkill = message.slice(0, maxLength);
+		return overkill + "..."
+	}
+}
 
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
